7ae2a3cd9e69e58be2e9e42835d7d47371f3e1f3
wuqian <102155038+wq2481@users.noreply.github.com>
2022-07-23 14:53:45 +0800

    Add files via upload

---
+++ 服务端.py
@@ -0,0 +1,18 @@
+# from socket import *
+# from matplotlib.pyplot import connect
+# from sqlalchemy import modifier
+# seversocket=socket(AF_INET,SOCK_STREAM)
+# seversocket.bind((gethostname(),5021))
+# print("准备接受信息")
+# for i in range(1,10):
+#     seversocket.listen(10)
+    
+#     consoc,adress=seversocket.accept()
+#     message = consoc.recv(1024).decode()
+#     print("客户端:"+message)
+#     mess=input("输入：")
+#     modifmessage=mess.encode()
+#     consoc.send(modifmessage)
+# seversocket.close()
+# #该机作为服务器
+# # 提供大小写转化
